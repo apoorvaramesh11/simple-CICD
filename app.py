d27d87e00cbc9622af95aa5cbf575ea5cf5c9eed
apoorvarameshac <apoorvaappu8892@gmail.com>
2026-02-25 13:56:23 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to My CI/CD Project 🚀"
+
+@app.route("/greet/<name>")
+def greet(name):
+    return f"Hello {name}! This app is deployed using Jenkins and Docker."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
